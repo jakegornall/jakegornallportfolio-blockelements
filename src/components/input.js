3e48d9b5425c8e72f82b2415abca6b5e53cd4756
Jacob Gornall <jgornall@dcec02w82pphv2h.limitedtoo.com>
2019-01-09 16:55:19 -0500

    Initial Commit

---
+++ src/components/input.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class Input extends React.Component {
+  render() {
+    return (
+      <input type={this.props.type} />
+    );
+  }
+}
+export default Input;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Input from './components/input';
+
+module.exports = {
+	Input
+}
